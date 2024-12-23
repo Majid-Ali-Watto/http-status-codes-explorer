ca4d5d91e3aeb40ec285dd3a22eeb5bd762a6b90
Majid Ali <majid@teresol.com>
2024-12-23 14:10:22 +0500

    SEO keywords updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import React, { createContext } from 'react';
+
+// Create a ThemeContext
+export const UrlContext = createContext({ url: '' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <UrlContext value={{ url: 'https://httpstatuscodesexplorer.vercel.app/' }}>
       <SpeedInsights />
       <Component {...pageProps} />
-    </>
+    </UrlContext>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import Header from "../components/Header";
 import CodeList from "../components/CodeList";
 import Footer from "@/components/Footer";
-import { useState } from "react";
+import { useState, use } from "react";
 import statusCodes from "@/assets/codes-list";
 import Head from "next/head";
+import { UrlContext } from "./_app";
 export default function Home() {
   const [filteredCode, setFilteredCode] = useState(statusCodes)
+  const { url } = use(UrlContext)
   const structuredData = {
     "@context": "https://schema.org",
     "@type": "WebPage",
     name: "HTTP Status Codes Explorer",
     description: "Explore HTTP status codes with descriptions and examples.",
-    url: "https://yourwebsite.com",
+    url,
   };
   return (
     <div
@@ -20,18 +22,18 @@ export default function Home() {
       <Head>
         <title>HTTP Status Codes Explorer</title>
         <meta name="description" content="Explore HTTP status codes with descriptions and examples. Filter, search, and download codes as PDF." />
-        <meta name="keywords" content="HTTP, Status Codes, HTTP Codes, Developer Tools" />
+        <meta name="keywords" content={"HTTP, Status Codes, HTTP Codes, Developer Tools,"+(statusCodes.map((code)=>code.code+' '+code.title+','+code.description+' '+code.example)).join(',')} />
         <meta name="author" content="Majid Ali - Full Stack Web Developer" />
         <meta property="og:title" content="HTTP Status Codes Explorer" />
         <meta property="og:description" content="Explore HTTP status codes with descriptions and examples. Filter, search, and download codes as PDF." />
         <meta property="og:image" content="/http-icon.png" />
-        <meta property="og:url" content="https://httpstatuscodesexplorer.vercel.app/" />
+        <meta property="og:url" content={url} />
         <meta name="twitter:card" content="summary_large_image" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
-        <link rel="canonical" href="https://httpstatuscodesexplorer.vercel.app/" />
+        <link rel="canonical" href={url} />
       </Head>
       <main className="min-h-[91dvh] flex flex-col gap-8">
         <Header setFilteredCode={setFilteredCode} filteredCode={filteredCode} />
